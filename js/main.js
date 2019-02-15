c04913e9341cad66c459cf9371e6691811f24d24
Eduardo Vieira <eduardofelipevieira@gmail.com>
2019-02-15 15:54:19 -0200

    UI Changes

--- js/main.js
+++ js/main.js
@@ -25,14 +25,18 @@
 	var ssPreloader = function() {
 
 		$WIN.on('load', function() {	
-
 			// force page scroll position to top at page refresh
-			$('html, body').animate({ scrollTop: 0 }, 'normal');
+				$('html, body').animate({ scrollTop: 0 }, 'normal');
 
          // will fade out the whole preloader DIV that covers the website.
-	      $("#preloader").delay(500).fadeOut('slow');
+				$("#preloader").delay(500).fadeOut('slow');
+
+			if(window.location.hash === '#download') {
+				$('[title=download]').click();
+			}
 	  
-	  	});
+		});	
+			
 	};
 
 
